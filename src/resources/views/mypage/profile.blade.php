931c5d7fa3b302a9cf05dd2e20ce0092ae9acdb3
naoya <n.hasepb0214@outlook.jp>
2026-04-28 22:35:00 +0900

    photo

--- src/resources/views/mypage/profile.blade.php
+++ src/resources/views/mypage/profile.blade.php
@@ -6,61 +6,57 @@
     <link rel="stylesheet" href="{{ asset('css/profile.css') }}">
 </head>
 <body>
-    <header class="header">
-        <div class="header__inner">
-            <a href="/">
-                <img src="{{ asset('images/coachtech.png') }}" alt="COACHTECHロゴ" class="header__logo">
-            </a>
 
-            <form action="/" method="GET" class="header__search-form">
-                <input type="text" name="keyword" class="header__search-input" placeholder="なにをお探しですか？">
+<header class="header">
+    <div class="header__inner">
+        <a href="/">
+            <img src="{{ asset('images/coachtech.png') }}" alt="COACHTECHロゴ" class="header__logo">
+        </a>
+
+        <form action="/" method="GET" class="header__search-form">
+            <input type="text" name="keyword" class="header__search-input" placeholder="なにをお探しですか？">
+        </form>
+
+        <nav class="header__nav">
+            <form action="/logout" method="POST">
+                @csrf
+                <button type="submit" class="header__nav-link header__logout">ログアウト</button>
             </form>
+            <a href="/mypage" class="header__nav-link">マイページ</a>
+            <a href="/sell" class="header__sell-button">出品</a>
+        </nav>
+    </div>
+</header>
 
-            <nav class="header__nav">
-                <form action="/logout" method="POST">
-                    @csrf
-                    <button type="submit" class="header__nav-link header__logout">ログアウト</button>
-                </form>
-                <a href="/mypage" class="header__nav-link">マイページ</a>
-                <a href="/sell" class="header__sell-button">出品</a>
-            </nav>
-        </div>
-    </header>
+<main class="mypage">
 
-    <main class="mypage">
-        <section class="profile">
-            <div class="profile__image"></div>
+    <section class="profile">
+        <div class="profile__image"></div>
+        <h1 class="profile__name">ユーザー名</h1>
+        <a href="/mypage/edit" class="profile__edit-button">プロフィールを編集</a>
+    </section>
 
-            <h1 class="profile__name">ユーザー名</h1>
+    <div class="tabs">
+        <a href="/mypage?tab=sell" class="tabs__item tabs__item--active">出品した商品</a>
+        <a href="/mypage?tab=buy" class="tabs__item">購入した商品</a>
+    </div>
 
-            <a href="/mypage/edit" class="profile__edit-button">
-                プロフィールを編集
-            </a>
-        </section>
-
-        <div class="tabs">
-            <a href="/mypage?tab=sell" class="tabs__item tabs__item--active">出品した商品</a>
-            <a href="/mypage?tab=buy" class="tabs__item">購入した商品</a>
-        </div>
-
+    {{-- 出品商品がある場合だけ表示 --}}
+    @if (isset($items) && $items->count())
         <section class="item-list">
-            {{-- 出品商品がある場合 --}}
-            @forelse ($items ?? [] as $item)
+            @foreach ($items as $item)
                 <div class="item-card">
-                    <img src="{{ asset('storage/' . $item->image) }}" alt="商品画像" class="item-card__image">
+                    @if (!empty($item->image))
+                        <img src="{{ $item->image }}" alt="{{ $item->name }}" class="item-card__image">
+                    @endif
+
                     <p class="item-card__name">{{ $item->name }}</p>
                 </div>
-
-            {{-- 出品商品がない場合 --}}
-            @empty
-                @for ($i = 0; $i < 7; $i++)
-                    <div class="item-card">
-                        <div class="item-card__dummy-image">商品画像</div>
-                        <p class="item-card__name">商品名</p>
-                    </div>
-                @endfor
-            @endforelse
+            @endforeach
         </section>
-    </main>
+    @endif
+
+</main>
+
 </body>
 </html>
